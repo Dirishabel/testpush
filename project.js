0d4721442e60c3601bea9470db54ed9556fb4231
Dmitryy Artemjev <artemjev42@gmail.com>
2020-12-01 00:39:59 +0500

    add tasks to pull

--- project.js
+++ project.js
@@ -1,5 +1,22 @@
 let but = document.getElementById('addtaskbutton');
+let tasklist = document.getElementById('tasklist');
 but.addEventListener('click', confirm, false)
 function confirm(){
-    alert('Hello world')
+    let newtaskitem = document.createElement('li');
+    let newtaskinfo = document.createElement('div');
+
+    let important = document.createElement('img');
+    important.src = 'images/fire.png';
+    important.className = 'fireicon';
+
+    let taskname = document.createElement('p');
+    let taskdeadline = document.createElement('p');
+    taskname.textContent = "Название: ";
+    taskdeadline.textContent = "Сдать до: ";
+    newtaskinfo.appendChild(important)
+    newtaskinfo.appendChild(taskname)
+    newtaskinfo.appendChild(taskdeadline)
+    newtaskinfo.className = 'taskitem'
+    newtaskitem.appendChild(newtaskinfo);
+    tasklist.appendChild(newtaskitem);
 }
